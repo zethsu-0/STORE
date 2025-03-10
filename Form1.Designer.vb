80b4802e3d59f3c0c231fa03453f4e838bc8f57f
Valdez_JSHTN <z3thsu0@gmail.com>
2025-03-10 21:54:43 +0800

    PRACTICE LANG

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,6 +23,10 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ItemnoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ProductnameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -41,8 +45,8 @@ Partial Class Form1
         Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.STOREDataSet2 = New STORE.STOREDataSet2()
         Me.CartBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.STOREDataSet2 = New STORE.STOREDataSet2()
         Me.CartTableAdapter = New STORE.STOREDataSet2TableAdapters.CartTableAdapter()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
@@ -52,24 +56,39 @@ Partial Class Form1
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.StocksBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.STOREDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.STOREDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.STOREDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.STOREDataSet2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.CartBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.STOREDataSet2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.AutoGenerateColumns = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ItemnoDataGridViewTextBoxColumn, Me.ProductnameDataGridViewTextBoxColumn, Me.QuantityDataGridViewTextBoxColumn, Me.PriceDataGridViewTextBoxColumn})
         Me.DataGridView1.DataSource = Me.StocksBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(80, 12)
+        Me.DataGridView1.Location = New System.Drawing.Point(80, 83)
         Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.Black
+        DataGridViewCellStyle2.ForeColor = System.Drawing.Color.White
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle2
         Me.DataGridView1.Size = New System.Drawing.Size(631, 161)
         Me.DataGridView1.TabIndex = 0
         '
@@ -123,7 +142,8 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(215, 191)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(215, 262)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(160, 56)
         Me.Button1.TabIndex = 1
@@ -132,19 +152,32 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(398, 210)
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(391, 292)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(180, 26)
         Me.TextBox1.TabIndex = 2
         '
         'DataGridView2
         '
         Me.DataGridView2.AutoGenerateColumns = False
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView2.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle3
         Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
         Me.DataGridView2.DataSource = Me.CartBindingSource
-        Me.DataGridView2.Location = New System.Drawing.Point(80, 253)
+        Me.DataGridView2.Location = New System.Drawing.Point(80, 424)
         Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        DataGridViewCellStyle4.BackColor = System.Drawing.Color.Black
+        DataGridViewCellStyle4.ForeColor = System.Drawing.Color.White
+        Me.DataGridView2.RowsDefaultCellStyle = DataGridViewCellStyle4
         Me.DataGridView2.Size = New System.Drawing.Size(631, 139)
         Me.DataGridView2.TabIndex = 3
         '
@@ -172,15 +205,15 @@ Partial Class Form1
         Me.DataGridViewTextBoxColumn4.HeaderText = "Price"
         Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
         '
-        'STOREDataSet2
-        '
-        Me.STOREDataSet2.DataSetName = "STOREDataSet2"
-        Me.STOREDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
         'CartBindingSource
         '
         Me.CartBindingSource.DataMember = "Cart"
         Me.CartBindingSource.DataSource = Me.STOREDataSet2
+        '
+        'STOREDataSet2
+        '
+        Me.STOREDataSet2.DataSetName = "STOREDataSet2"
+        Me.STOREDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'CartTableAdapter
         '
@@ -188,39 +221,45 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(305, 409)
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(305, 580)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(158, 26)
         Me.TextBox2.TabIndex = 4
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(305, 452)
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(305, 623)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(158, 26)
         Me.TextBox3.TabIndex = 5
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(96, 407)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(96, 578)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.Size = New System.Drawing.Size(44, 20)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Total"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(96, 459)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(96, 630)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.Size = New System.Drawing.Size(65, 20)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Amount"
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(303, 494)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(303, 665)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(160, 30)
         Me.Button2.TabIndex = 8
@@ -229,14 +268,16 @@ Partial Class Form1
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(305, 546)
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(305, 717)
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox4.Size = New System.Drawing.Size(158, 26)
         Me.TextBox4.TabIndex = 9
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(303, 581)
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(303, 752)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(160, 30)
         Me.Button3.TabIndex = 10
@@ -246,17 +287,52 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(96, 553)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(96, 724)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.Size = New System.Drawing.Size(65, 20)
         Me.Label3.TabIndex = 11
         Me.Label3.Text = "Change"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(387, 262)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(184, 20)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "ENTER ITEM_NUMBER"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(332, 384)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(108, 37)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "CART"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(309, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(154, 37)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "STOCKS"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 614)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 822)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.TextBox4)
@@ -278,8 +354,8 @@ Partial Class Form1
         CType(Me.STOREDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.STOREDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.STOREDataSet2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.CartBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.STOREDataSet2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -314,4 +390,7 @@ Partial Class Form1
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents Button3 As Button
     Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Reflection.Emit
 
 Public Class Form1
 
@@ -11,6 +12,8 @@ Public Class Form1
 
         CartItems()
         StocksItems()
+        resetstock()
+        InitializeStock()
     End Sub
 
     Public Sub StocksItems()
@@ -40,14 +43,52 @@ Public Class Form1
             End Using
         End Using
     End Sub
+    Public Sub resetstock()
+        Dim query As String = "DELETE FROM Cart"
+        Using cmd As SqlCommand = New SqlCommand(query, con)
+            con.Open()
+            cmd.ExecuteNonQuery()
+            con.Close()
+        End Using
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = ""
+        CartItems()
+
+    End Sub
+
+    Public Sub InitializeStock()
+        Opencon()
+
+        Dim stockData As New Dictionary(Of String, Integer) From {
+        {"1", 10},
+        {"2", 20},
+        {"3", 30},
+        {"4", 40},
+        {"5", 50}
+    }
+
+        For Each item In stockData
+            Dim query As String = "UPDATE Stocks SET Quantity = @Quantity WHERE Item_no = @Item_no"
+            Using cmd As New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@Quantity", item.Value)
+                cmd.Parameters.AddWithValue("@Item_no", item.Key)
+                cmd.ExecuteNonQuery()
+            End Using
+        Next
+
+        con.Close()
+    End Sub
+
+
+
+    Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Opencon()
 
         Dim Item_no As String = TextBox1.Text
         Dim Product_name As String = String.Empty
         Dim Quantity As Integer = 0
         Dim Price As Decimal = 0
+
 
         Dim checkQuery As String = "SELECT Product_name, Quantity, Price FROM stocks WHERE Item_no = @Item_no"
         Using selectCmd As New SqlCommand(checkQuery, con)
@@ -68,54 +109,54 @@ Public Class Form1
         End Using
 
 
-        Dim checkCartQuery As String = "SELECT Quantity, Price FROM Cart WHERE Item_No = @Item_no"
-        Using checkCartCmd As New SqlCommand(checkCartQuery, con)
-            checkCartCmd.Parameters.AddWithValue("@Item_no", Item_no)
-
-            Dim existingQuantity As Integer = 0
-            Dim existingPrice As Decimal = 0
-            Dim cartReader As SqlDataReader = checkCartCmd.ExecuteReader()
-
-            If cartReader.Read() Then
-
-                existingQuantity = Convert.ToInt32(cartReader("Quantity"))
-                existingPrice = Convert.ToDecimal(cartReader("Price"))
-                cartReader.Close()
+        If Quantity <= 0 Then
+            MsgBox("Out of stock!", vbExclamation)
+            con.Close()
+            Return
+        End If
 
 
-                Dim newQuantity As Integer = existingQuantity + 1
-                Dim TotalPrice As Decimal = newQuantity * Price
+        Dim cartQuantity As Integer = 0
+        Dim checkCartQuery As String = "SELECT Quantity FROM Cart WHERE Item_No = @Item_no"
+        Using cmd As New SqlCommand(checkCartQuery, con)
+            cmd.Parameters.AddWithValue("@Item_no", Item_no)
+            Using reader As SqlDataReader = cmd.ExecuteReader()
+                If reader.Read() Then
+                    cartQuantity = Convert.ToInt32(reader("Quantity")) + 1
+                End If
+            End Using
+        End Using
 
+        If cartQuantity > 0 Then
+            Dim updateQuery As String = "UPDATE Cart SET Quantity = @Quantity, Price = @Price WHERE Item_No = @Item_no"
+            Using updateCmd As New SqlCommand(updateQuery, con)
+                updateCmd.Parameters.AddWithValue("@Quantity", cartQuantity)
+                updateCmd.Parameters.AddWithValue("@Price", cartQuantity * Price)
+                updateCmd.Parameters.AddWithValue("@Item_no", Item_no)
+                updateCmd.ExecuteNonQuery()
+            End Using
+        Else
+            Dim insertQuery As String = "INSERT INTO Cart (Item_No, Product_name, Quantity, Price) VALUES (@Item_no, @Product_name, @Quantity, @Price)"
+            Using insertCmd As New SqlCommand(insertQuery, con)
+                insertCmd.Parameters.AddWithValue("@Item_no", Item_no)
+                insertCmd.Parameters.AddWithValue("@Product_name", Product_name)
+                insertCmd.Parameters.AddWithValue("@Quantity", 1)
+                insertCmd.Parameters.AddWithValue("@Price", Price)
+                insertCmd.ExecuteNonQuery()
+            End Using
+        End If
 
-                Dim updateQuery As String = "UPDATE Cart SET Quantity = @NewQuantity, Price = @TotalPrice WHERE Item_No = @Item_no"
-                Using updateCmd As New SqlCommand(updateQuery, con)
-                    updateCmd.Parameters.AddWithValue("@NewQuantity", newQuantity)
-                    updateCmd.Parameters.AddWithValue("@TotalPrice", TotalPrice)
-                    updateCmd.Parameters.AddWithValue("@Item_no", Item_no)
-                    updateCmd.ExecuteNonQuery()
-                End Using
-            Else
-
-                cartReader.Close()
-
-                Dim totalPrice As Decimal = 1 * Price
-
-                Dim insertQuery As String = "INSERT INTO Cart (ItemNo, Product_name, Quantity, Price) VALUES (@Item_no, @Product_name, @Quantity, @Price)"
-                Using insertCmd As New SqlCommand(insertQuery, con)
-                    insertCmd.Parameters.AddWithValue("@Item_no", Item_no)
-                    insertCmd.Parameters.AddWithValue("@Product_name", Product_name)
-                    insertCmd.Parameters.AddWithValue("@Quantity", 1)
-                    insertCmd.Parameters.AddWithValue("@Price", totalPrice)
-                    insertCmd.ExecuteNonQuery()
-                End Using
-            End If
-
+        Dim updateStockQuery As String = "UPDATE Stocks SET Quantity = Quantity - 1 WHERE Item_no = @Item_no"
+        Using stockCmd As New SqlCommand(updateStockQuery, con)
+            stockCmd.Parameters.AddWithValue("@Item_no", Item_no)
+            stockCmd.ExecuteNonQuery()
         End Using
 
         MsgBox("Item added to Cart successfully.", vbInformation)
         TextBox1.Text = ""
 
         CartItems()
+        StocksItems()
 
         Dim totalCartPrice As Decimal = 0
         Dim totalPriceQuery As String = "SELECT SUM(Price) AS TotalPrice FROM Cart"
@@ -128,6 +169,51 @@ Public Class Form1
         End Using
 
         TextBox2.Text = totalCartPrice
+
         con.Close()
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        resetstock()
+        CartItems()
+        StocksItems()
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        MessageBox.Show("Cart cleared successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Public Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim forSukli As Integer = TextBox3.Text - TextBox2.Text
+
+        If Integer.TryParse(TextBox3.Text, Nothing) Then
+            If TextBox3.Text >= TextBox2.Text Then
+                TextBox4.Text = forSukli
+            Else
+                MessageBox.Show("Kulanggg")
+            End If
+        Else
+            MessageBox.Show("lagyan mo boss")
+        End If
+
+
+    End Sub
+    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) AndAlso e.KeyChar <> "."c Then
+            e.Handled = True
+        End If
+
+        If e.KeyChar = "."c AndAlso TextBox2.Text.Contains(".") Then
+            e.Handled = True
+        End If
+    End Sub
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) AndAlso e.KeyChar <> "."c Then
+            e.Handled = True
+        End If
+
+        If e.KeyChar = "."c AndAlso TextBox2.Text.Contains(".") Then
+            e.Handled = True
+        End If
+    End Sub
+
 End Class
